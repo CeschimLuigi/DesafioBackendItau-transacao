b99bb8bac7a73060dea7c4bdceb8efedf3fd2763
CeschimLuigi <luigifaggeceschim@gmail.com>
2025-01-28 13:51:16 -0300

    SwaggerConfig, Dependências pom xml, alteração na documentação da EstatisticaController.

--- src/main/java/com/luigiceschim/api_transacao/controller/EstatisticaController.java
+++ src/main/java/com/luigiceschim/api_transacao/controller/EstatisticaController.java
@@ -2,6 +2,9 @@ package com.luigiceschim.api_transacao.controller;
 
 import com.luigiceschim.api_transacao.dto.EstatisticaResponseDTO;
 import com.luigiceschim.api_transacao.service.EstatisticaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,13 @@ public class EstatisticaController {
     private EstatisticaService service;
 
     @GetMapping
+    @Operation(description = "Endpoint responsavel por buscar estatisticas")
+    @ApiResponses(value ={
+            @ApiResponse(responseCode = "200", description = "Estatisticas obtidas com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro de busca de estatistic"),
+            @ApiResponse(responseCode = "500",description = "Erro interno do servidor")
+
+    })
     public ResponseEntity<EstatisticaResponseDTO> obterEstatistica(@RequestParam(value = "intervaloBusca", required = false, defaultValue = "60") Integer intervaloBusca){
         var result = service.obterEstatistica(intervaloBusca);
         return ResponseEntity.status(HttpStatus.OK).body(result);
--- src/main/java/com/luigiceschim/api_transacao/controller/TransacaoController.java
+++ src/main/java/com/luigiceschim/api_transacao/controller/TransacaoController.java
@@ -3,6 +3,9 @@ package com.luigiceschim.api_transacao.controller;
 import com.luigiceschim.api_transacao.dto.EstatisticaResponseDTO;
 import com.luigiceschim.api_transacao.dto.TransacaoRequestDTO;
 import com.luigiceschim.api_transacao.service.TransacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,13 +19,28 @@ public class TransacaoController {
     private TransacaoService service;
 
     @PostMapping
+    @Operation(description = "Endpoint responsavel por adicionar transações")
+    @ApiResponses(value ={
+            @ApiResponse (responseCode = "201", description = "Transação gravada com sucesso"),
+            @ApiResponse(responseCode = "422",description = "Campos não atendem os requisitos da transação"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500",description = "Erro interno do servidor")
+
+    })
     public ResponseEntity<Void> criarTransacao(@RequestBody TransacaoRequestDTO requestDTO){
         service.adicionarTransacoes(requestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping
-    public ResponseEntity<EstatisticaResponseDTO> deletarTransacoes(){
+    @Operation(description = "Endpoint responsavel por deletar transações")
+    @ApiResponses(value ={
+            @ApiResponse (responseCode = "200", description = "Transação deletadas com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500",description = "Erro interno do servidor")
+
+    })
+    public ResponseEntity<Void> deletarTransacoes(){
         service.deletarTransacoes();
         return ResponseEntity.status(HttpStatus.OK).build();
     }
